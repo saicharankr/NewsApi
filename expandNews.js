e63b6c95a28e5f7c044f40d6935e7e0bc4b0da8d
saicharankr <saicharankr@gmail.com>
2021-03-13 01:56:21 -0800

    fullNewsIndianExpress

--- expandNews.js
+++ expandNews.js
@@ -2,18 +2,29 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 
-const fullNews = async (url,source) => {
+const fullNews = async (url, source) => {
     try {
         const { data } = await axios.get(
             url
         );
         const $ = cheerio.load(data);
-        if(source == "indianexpress"){
-            console.log($("h1.native_story_title").text())
-            console.log($("h2.synopsis").text())
-            $("#pcl-full-content > p").each((id,el)=> {
-                console.log(el)
+        if (source == "indianexpress") {
+
+            var content = []
+
+            $("#pcl-full-content > p").each((id, el) => {
+                if ( el.firstChild !== null  && el.firstChild.data !== undefined) {
+                    content.push(el.firstChild.data)
+                }
             })
+
+            var story = {
+                "title": $("h1.native_story_title").text(),
+                "synopsis": $("h2.synopsis").text(),
+                "content": content
+
+            }
+            return story
         }
 
     } catch (err) {
@@ -21,4 +32,4 @@ const fullNews = async (url,source) => {
     }
 }
 
-fullNews("https://indianexpress.com/article/india/ashwani-kumar-interview-on-farmers-protests-nda-govt-congress-7226331/","indianexpress")
+fullNews("https://indianexpress.com/article/india/ashwani-kumar-interview-on-farmers-protests-nda-govt-congress-7226331/", "indianexpress").then(data => console.log(data))
