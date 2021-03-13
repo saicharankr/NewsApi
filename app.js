4a1005de15beef76faa1927361275acdb116c6a6
saicharankr <saicharankr@gmail.com>
2021-03-13 02:22:06 -0800

    added pagination

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-const {getNdtvNews,indiaTodayNews,getTinNews}=require('./newsScraper')
+const { getNdtvNews, indiaTodayNews, getTinNews } = require('./newsScraper')
 const express = require("express")
+const paginate = require('jw-paginate')
 
 
-const app=express()
 
-app.get("/ndtv",(req,res)=>{
-    getNdtvNews().then(list => res.send(list))    
+
+const app = express()
+
+app.get("/ndtv", (req, res) => {
+    getNdtvNews().then(function (list) {
+        const page = parseInt(req.query.page) || 1;
+
+        // get pager object for specified page
+        const pageSize = 10;
+        const pager = paginate(list.length, page, pageSize);
+
+        // get page of items from items array
+        const pageOfItems = list.slice(pager.startIndex, pager.endIndex + 1);
+
+        // return pager object and current page of items
+        return res.json({ pager, pageOfItems });
+    })
 })
 
 
-app.get("/in",(req,res)=>{
-    indiaTodayNews().then(list => res.send(list))    
+app.get("/in", (req, res) => {
+    indiaTodayNews().then(function (list) {
+        const page = parseInt(req.query.page) || 1;
+
+        // get pager object for specified page
+        const pageSize = 10;
+        const pager = paginate(list.length, page, pageSize);
+
+        // get page of items from items array
+        const pageOfItems = list.slice(pager.startIndex, pager.endIndex + 1);
+
+        // return pager object and current page of items
+        return res.json({ pager, pageOfItems });
+
+    })
 })
 
-app.get("/tin",(req,res)=>{
-    getTinNews().then(list => res.send(list))    
+app.get("/tin", (req, res) => {
+    getTinNews().then(function (list) {
+        const page = parseInt(req.query.page) || 1;
+
+        // get pager object for specified page
+        const pageSize = 10;
+        const pager = paginate(list.length, page, pageSize);
+
+        // get page of items from items array
+        const pageOfItems = list.slice(pager.startIndex, pager.endIndex + 1);
+
+        // return pager object and current page of items
+        return res.json({ pager, pageOfItems });
+
+    })
 })
 
 app.listen(8080)
